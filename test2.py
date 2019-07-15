c4888404daa0ac1068869feb56e2805d14f5599d
Vamshi Krishna Dontheneni <Vamshi.Dontheneni@bestbuy.com>
2019-07-15 15:34:58 -0500

    updating new urls

--- test2.py
+++ test2.py
@@ -152,11 +152,8 @@ def check_app(appname):
 
 
 
-# if ( `git status --porcelain` ):
-#     print "commiting"
-# if not ( os.system("git status --porcelain") ):
-    # os.system("git add .")
-    # os.system("git commit -m 'updating new urls' ")
-    # os.system("git push https://vamshi0411:cricket18@github.com/vamshi0411/versionmatch.git --all")
-test = os.system("git status --porcelain")
-print test
+
+os.system("git status --porcelain")
+os.system("git add .")
+os.system("git commit -m 'updating new urls' ")
+os.system("git push https://vamshi0411:cricket18@github.com/vamshi0411/versionmatch.git --all")
